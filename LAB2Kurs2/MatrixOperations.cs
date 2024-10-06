264c7ac9d2febb2779c7a315d08f83d472e60e2b
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-10-06 15:39:47 +0300

    matrix addition

--- LAB2Kurs2/MatrixOperations.cs
+++ LAB2Kurs2/MatrixOperations.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,22 @@ namespace LAB2Kurs2
 {
     partial class MyMatrix
     {
-
+        public static MyMatrix operator +(MyMatrix m1,MyMatrix m2)
+        {
+            if (m1.Heigth == m2.Heigth && m1.Weidth == m2.Weidth)
+            {
+                MyMatrix mres = new MyMatrix();
+                mres.matrix = new double[m1.Heigth, m1.Weidth];
+                for (int i = 0; i < m1.Heigth; i++)
+                {
+                    for (int j = 0; j < m1.Weidth; j++)
+                    {
+                        mres[i, j] = m1[i, j] + m2[i, j];
+                    }
+                }
+                return mres;
+            }
+            else throw new Exception("Different sizes");
+        }
     }
 }
